be4cbc45963db6546aae302f8e6986bca7d59385
Timothy Do <tydo83@gmail.com>
2020-09-24 02:54:13 -0400

    Passed every tests

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(str) {
+  if(str.length > 20) {
+    return 'That\'s a long string!';
+  }
+}
 
+function isItMedium(str) {
+  if(str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!";
+  }
+}
 
+function isItShort(str) {
+  if(str.length < 10) {
+    return "That's a small string!";
+  }
+  else 
+    return "That's not a small string!";
+}
 
+function howLongIsMyString(str) {
+  if(str.length > 20) {
+    return 'That\'s a long string!';
+  }
+  else if(str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!";
+  }
+  else {
+    return "That's a small string!";
+  }
+}
 
-
+function instructorHeight(str) {
+  if(str === 'Colin') {
+    return 62;
+  }
+  else if(str === 'Mesuara') {
+    return 69;
+  }
+  else {
+    return "I don't know that instructor!";
+  }
+}
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
